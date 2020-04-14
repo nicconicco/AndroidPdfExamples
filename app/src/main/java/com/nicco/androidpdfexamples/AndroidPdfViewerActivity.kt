f209777261f7675e2f0b24ab8912e6a341233979
Carlos Nicolau Galves <nicolaugalves@gmail.com>
2020-04-14 10:03:39 -0300

    testing

--- app/src/main/java/com/nicco/androidpdfexamples/AndroidPdfViewerActivity.kt
+++ app/src/main/java/com/nicco/androidpdfexamples/AndroidPdfViewerActivity.kt
@@ -1,10 +1,17 @@
 package com.nicco.androidpdfexamples
 
+import android.Manifest
+import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.provider.SyncStateContract
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.FileProvider
+import androidx.core.content.FileProvider.getUriForFile
 import kotlinx.android.synthetic.main.activity_android_pdf_viewer.*
+import java.io.File
+import java.security.AccessController.getContext
 
 
 class AndroidPdfViewerActivity : AppCompatActivity() {
@@ -24,7 +31,38 @@ class AndroidPdfViewerActivity : AppCompatActivity() {
         }
 
         btnShare.setOnClickListener {
+            val imagePath = File(filesDir, "assets")
+            val newFile = File(imagePath, "extend_pdf.pdf")
+            val contentUri =
+                getUriForFile(this, "com.nicco.androidpdfexamples", newFile)
 
+            val sendIntent: Intent = Intent().apply {
+                action = Intent.ACTION_SEND
+                addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+                putExtra(Intent.EXTRA_STREAM, contentUri)
+                type = "application/pdf"
+            }
+
+            val shareIntent = Intent.createChooser(sendIntent, null)
+            startActivity(shareIntent)
         }
     }
 }
+
+//fun getByImage(context: Context, file: File) {
+//    FileProvider.getUriForFile(
+//        context,
+//        SyncStateContract.Constants.PACKAGE_FILE_PROVIDER,
+//        file
+//    )?.let {
+//        with(Intent()) {
+//            action = Intent.ACTION_SEND
+//            addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+//            setDataAndType(it, context.contentResolver.getType(it))
+//            type = context.contentResolver?.getType(it)
+//            putExtra(Intent.EXTRA_STREAM, it)
+//            context.startActivity(Intent.createChooser(this, null))
+//        }
+//    }
+//}
+//}
